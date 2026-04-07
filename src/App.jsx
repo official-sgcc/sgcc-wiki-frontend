87e28fe86da7e365b20e7ac979270bc13596c568
skyflowlab <179109559+skyflowlab@users.noreply.github.com>
2026-04-07 18:11:19 +0900

    feat: route 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MyPage from './MyPage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   )
