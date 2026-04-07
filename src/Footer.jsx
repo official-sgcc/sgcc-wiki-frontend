7121812f5f6ef4bd6d5c4781e5ab6c8540d2f8f4
busicoder <judge060217@gmail.com>
2026-04-07 17:05:28 +0900

    feat: add Footer and SearchMordal

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+import githubLogo from './assets/github.svg'
+import instaLogo from './assets/insta.png'
+
+function Footer() {
+
+  return (
+    <footer>
+        <div className="FooterContainer">
+            <a href="https://github.com/Sogang-Computer-Club" target="_blank"><img className="logo" src={githubLogo} alt="GitHub" />SGCC 공식 GitHub</a>
+            <a href="https://www.instagram.com/sgcc_sogang/" target="_blank"><img className="logo" src={instaLogo} alt="Insta" />SGCC 공식 인스타</a>
+        </div>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/SearchMordal.jsx
@@ -0,0 +1,48 @@
+import './SearchMordal.css'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SearchModal({ onClose }) {
+    const [keyword,setKeyword]=useState("");
+    const navigate=useNavigate();
+
+    useEffect(() => {
+        // 스크롤 막기
+        document.body.style.overflow = "hidden";
+
+        // 모달 닫히면 복구
+        return () => {
+        document.body.style.overflow = "auto";
+        };
+    }, []);
+    const handleKeyDown=(e)=>{//enter키 처리
+        if(e.key=='Enter'){
+            handleSearch();
+        }
+    };
+
+    //트래픽 감당되면 onChange에도 handleSearch 달기
+    const handleSearch=()=>{//검색 query 보낼 함수
+        var txt=keyword.trim();
+        if(!txt){
+            alert('검색어를 입력해주세요.');
+            return;
+        }
+        alert(txt+'제출됨');
+        navigate(`/search?q=${txt}`);
+        onClose();
+    };
+
+    return (
+        <div className="modal_overlay" onClick={onClose}>
+            <div className="mordal_content" onClick={(e)=>e.stopPropagation()}>
+                <div className='srchbox'>
+                    <input id='srchinput' type="text" onChange={(e)=>setKeyword(e.target.value)} placeholder='검색어를 입력하세요' onKeyDown={handleKeyDown}/>
+                    <button className='srchbtn' onClick={handleSearch} />
+                </div>
+            </div>
+        </div>
+    );
+}
+export default SearchModal
