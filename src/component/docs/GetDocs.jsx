d7c1cd9f639cab11074c7fa86be3464748a07959
busicoder <judge060217@gmail.com>
2026-05-10 18:52:19 +0900

    chore: md 처리, md 패키지 추가, 헤더 색 변경

--- src/component/docs/GetDocs.jsx
+++ src/component/docs/GetDocs.jsx
@@ -2,59 +2,71 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import NotFound from "./NotFound";
+import ReactMarkdown from "react-markdown";
 
 const api_url = import.meta.env.VITE_SERVER_URL;
 async function getDocsData(title) {
   try {
     const response = await axios.get(`${api_url}/documents/${title}`);
-    return convertToHTML(response.data);
+    return {
+      ok : true,
+      data : (response).data,
+    };
   } catch (e) {
-    if (e.response) {
-      console.log(`status code : ${e.response.status}`);
-    } else console.error(e);
-    return <NotFound status={e.response.status} message="문서를 찾을 수 없습니다"/>;
+    return {
+      ok: false,
+      status : e.response.status,
+    }
   }
 }
 async function createDocs(Title, Content, Tags) {
   try {
-    await axios.post(`${api_url}/documents`,{params: {
-      title: Title,
-      content : Content,
-      tags : Tags
-    }});
+    await axios.post(`${api_url}/documents`, {
+      params: {
+        title: Title,
+        content: Content,
+        tags: Tags,
+      },
+    });
   } catch (e) {
     console.error(e);
   }
-}
-function convertToHTML(doc){
-  return (
-  <article>
-    <h1>{doc.title}</h1>
-    <h1>{doc.content}</h1>
-    <div>
-      {doc.tags.map((tag) => (
-        <span key={tag}>
-          #{tag}
-        </span>
-      ))}
-    </div>
-  </article>
-  );
 }
 
 function GetDocs() {
   const { title } = useParams();
   const [doc, setDoc] = useState(null);
+  const [loding,setLoding] = useState(true);
   useEffect(() => {
     async function fetchDoc() {
+      setLoding(true);
       const data = await getDocsData(title);
       setDoc(data);
+      setLoding(false);
     }
     fetchDoc();
   }, [title]);
+
+  if(loding){
+    return (
+      //loding 창 들어갈곳
+      //임시로 notfound로 땜빵
+      <NotFound status={0} message="검색 중 . . ."/>
+    );
+  }
+  if(!doc.ok){
+    return (
+      <NotFound status={doc.status} message="문서를 찾을 수 없습니다"/>
+    );
+  }
+
+  //doc.data에 title, content, 날짜 등이 있음
   return (
     <article>
-      {doc !== null ? doc : <NotFound status={0} message="검색 중 . . . "/>}
+      <h1>{doc.data.title}</h1>
+      <ReactMarkdown>
+        {doc.data.content}
+      </ReactMarkdown>
     </article>
   );
 }
