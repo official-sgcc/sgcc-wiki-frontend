3296e471c4de87c63e53f824a4f8e1b790a53b66
busicoder <judge060217@gmail.com>
2026-07-14 15:35:31 +0900

    feat: 임시 TagList 구현. 페이지 넘기기 구현 안 됨

--- src/component/docs/Tagview/TagList.jsx
+++ src/component/docs/Tagview/TagList.jsx
@@ -1,11 +1,54 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import DocsList from "../../layout/DocsList";
+import { GetDocsFromTag } from "../../util/TagCategoryAPI";
+
+//해야하는 과업: limit과 offset 이용해서 페이지 넘기기 구현하기
 
 export default function TagList(){
-    const {tag} = useParams();
-    
+
+    const { tag } = useParams();
+
+    const [docs, setDocs] = useState([]);
+
+
+    useEffect(() => {
+
+        async function load(){
+
+            try{
+
+                const response = await GetDocsFromTag(tag);
+
+                setDocs(response);
+
+            }
+            catch(e){
+
+                console.error(e);
+                setDocs([]);
+
+            }
+
+        }
+
+
+        load();
+
+    }, [tag]);
+
+
     return (
-        <>
-        </>
+        <div>
+            <h2>
+                #{tag}
+            </h2>
+
+            <DocsList
+                docsdata={docs}
+            />
+        </div>
     );
 
 }
