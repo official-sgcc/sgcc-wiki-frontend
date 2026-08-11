8523030611b1d1b3197ed3d0233b5d8a1a09a097
busicoder <judge060217@gmail.com>
2026-08-11 15:39:09 +0900

    feat: 지원서 구글 폼 연결

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import MakeAccount from './component/account/MakeAccount'
 import TagList from './component/docs/tagview/TagList'
 import WelcomePage from './component/page/welcome/WelcomePage'
 import AdminPage from './component/page/admin/AdminPage'
+import { RecruitForm } from './component/page/welcome/Recruit'
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
         <Route path="/signup" element={<Layout content={<MakeAccount />}/>} />
         <Route path="/tag/:tag" element={<Layout content={<TagList />}/>} />
         <Route path="/welcome" element={<Layout content={<WelcomePage />}/>} />
+        <Route path="/recruit" element={<Layout content={<RecruitForm />}/>} />
         <Route path="/admin" element={<Layout content={<AdminPage />}/>} />
         <Route path="*" element={<Layout content={<NotFound />}/>} />
       </Routes>
---
+++ src/component/page/welcome/Recruit.jsx
@@ -0,0 +1,5 @@
+export function RecruitForm(){
+    return (
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdD17VyY8w0acVah_llAJIxSQjjXObbYqxdDZD_4sj08ZF1xA/viewform?embedded=true" width="640" height="5460" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>
+    );
+}
