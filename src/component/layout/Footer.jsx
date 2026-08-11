63be5f1328433c657fa7b6d94ada705e34d5d397
suepsuep <inoeul1222@naver.com>
2026-08-11 14:13:55 +0900

    fix: footer design

--- src/component/layout/Footer.jsx
+++ src/component/layout/Footer.jsx
@@ -1,14 +1,20 @@
 import './Footer.css'
 import githubLogo from '../../assets/github.svg'
 import instaLogo from '../../assets/insta.png'
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 function Footer() {
 
   return (
     <footer>
         <div className="FooterContainer">
-            <a href="https://github.com/Sogang-Computer-Club" target="_blank"><img className="logo" src={githubLogo} alt="GitHub" />SGCC 공식 GitHub</a>
-            <a href="https://www.instagram.com/sgcc_sogang/" target="_blank"><img className="logo" src={instaLogo} alt="Insta" />SGCC 공식 Instagram</a>
+            <a href="https://github.com/Sogang-Computer-Club" target="_blank">
+              <FaGithub className='btn'/>GitHub
+            </a>
+            <a href="https://www.instagram.com/sgcc_sogang/" target="_blank">
+              <FaInstagram className='btn'/>Instagram
+            </a>
         </div>
     </footer>
   )
