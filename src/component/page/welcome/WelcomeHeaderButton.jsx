88efb4172546014c14535ddc3f8eb710e1f52e8c
busicoder <judge060217@gmail.com>
2026-08-31 17:18:51 +0900

    fix: extend recruitment period through September 4

--- src/component/page/welcome/WelcomeHeaderButton.jsx
+++ src/component/page/welcome/WelcomeHeaderButton.jsx
@@ -4,7 +4,7 @@ import './WelcomeHeaderButton.css'
 function WelcomeHeaderButton() {
     return (
         <Link to='/welcome' className='welcomebutton'>
-            🎉 신입부원 모집중! (~9/10)
+            🎉 신입부원 모집중! (~9/4)
         </Link>
     )
 }
